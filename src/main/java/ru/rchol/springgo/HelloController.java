ff61689848a242e4767c72dc68116c3655db09ec
rchol <>
2018-06-13 01:57:58 +0300

    Отредактирован запрос /hello (добавлена запись в БД) Добавлен GET запрос на получение всех записей таблицы visitors Добавлен POST запрос на запись пользователся в таблицу visitors

--- src/main/java/ru/rchol/springgo/HelloController.java
+++ src/main/java/ru/rchol/springgo/HelloController.java
@@ -1,9 +1,15 @@
 package ru.rchol.springgo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.rchol.springgo.model.Visitors;
+import ru.rchol.springgo.modelwork.VisitorsService;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
@@ -14,9 +20,25 @@ public class HelloController {
     private static final String message = "GET by %s";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private VisitorsService visitorsService;
+
     @RequestMapping(value="/hello", method=GET)
     public Hello hello(@RequestParam(value="name", defaultValue="You") String name){
+        visitorsService.addVisitor(new Visitors(name));
         return new Hello(counter.incrementAndGet(), String.format(message, name));
     }
+
+    @RequestMapping(value = "/show", method = GET)
+    public @ResponseBody ResponseEntity<List<Visitors>> getAll() {
+        return ResponseEntity.ok(visitorsService.getAllVisitors());
+    }
+
+    @RequestMapping(value = "/addVisitor", method = POST)
+    public void addVisitorApi(@RequestParam(value = "name", defaultValue = "Anon") String name){
+        visitorsService.addVisitor(new Visitors(name));
+        counter.incrementAndGet();
+    }
+
 
 }
